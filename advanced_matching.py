f9c0b756fc1e925e03499fd933426b2a6149d7ef
Makara Laszlo Arpad <makara.laszlo.arpad@gmail.com>
2023-10-30 17:43:07 +0100

    doc: advanced_macthing documentated refactor: few modifications on the code style made feat: gitignore added to source tree

--- advanced_matching.py
+++ advanced_matching.py
@@ -1,12 +1,14 @@
 from typing import Tuple
 
 
-# TODO megvalositani valahogy hogy grafikusan megjelnjenek az egyes binaris fak
-#   veszelyes tud lenni mert ha oriasi nagy akkor 2^32-en lesz
-# https://towardsdatascience.com/implementing-a-trie-data-structure-in-python-in-less-than-100-lines-of-code-a877ea23c1a1
-class Lulea_node():
-    # Root letrehozasa az adott bufferhez
-    def __init__(self, char:str):
+class Lulea_node:
+    """
+    Lulea node for the trie.
+    This implementation is based on the following article:
+    https://towardsdatascience.com/implementing-a-trie-data-structure-in-python-in-less-than-100-lines-of-code-a877ea23c1a1
+    """
+
+    def __init__(self, char: str) -> None:
         self.char = char
         self.children = []
         self.word_finished = False
@@ -14,14 +16,20 @@ class Lulea_node():
         self.counter = 1
 
 
-def add(root, word:str):
-    # egy ip cim hozzadasa az adott fahoz
+def add(root, word: str) -> None:
+    """
+    Adding an IP address to the tree.
+
+    :param root: the root of the trie
+    :param word: given IP address
+    :return: None
+    """
     node = root
     for char in word:
         found_in_child = False
         for child in node.children:
             if child.char == char:
-                child.counter+=1
+                child.counter += 1
                 node = child
                 found_in_child = True
                 break
@@ -32,39 +40,40 @@ def add(root, word:str):
     node.word_finished = True
 
 
-def delete(root, word:str):
-    # Bool, int Tuple formatumban allnak elo az adatok
-    # meg kell nezni megvan-e a prefix a faban, ha megvan akkor
-    # vegig kell szaladni az egyes child nodeokon es -1 el kell csokkenteni
-    # az adott node counterjet, abban az esetben ha ekkor a node counterje 0-ra
-    # csokkenne akkor az adott nodeot torolni kell
+def delete(root, word: str) -> None:
+    """
+    Deleting an IP address from the tree.
 
-    # megnezzuk benne van-e, ez vissza kell terjen True,1-el
+    :param root: root of the trie
+    :param word: given IP address that should be deleted
+    :return: None
+    """
     answ = find_prefix(root=root, prefix=word)
-
-    # ha True ertekkel tert vissza akkor
-    if answ[0] == True:
+    if answ[0]:
         node = root
-
         for char in word:
             found_in_child = False
             for child in node.children:
-                # countereket csokkentjuk 1-el, ha 0-at elerik ki kell oket torolni
                 if child.char == char:
                     child.counter -= 1
-                    # ha 0 akkor mindegy mi van alatta mindent ki lehet dobni hiszen ez az egy rekord volt
                     if child.counter == 0:
                         node.children.remove(child)
-                    # hanem akkor meg megyunk tovabb
                     node = child
                     found_in_child = True
                     break
 
 
-def find_prefix(root, prefix:str) -> Tuple[bool, int]:
+def find_prefix(root, prefix: str) -> Tuple[bool, int]:
+    """
+    Finding the given prefix in the tree.
+
+    :param root: root of the trie
+    :param prefix: prefix that should be found in the tree
+    :return: state of the found, number of the occurrences
+    """
     node = root
     if not root.children:
-        return False,0
+        return False, 0
     for char in prefix:
         char_not_found = True
         for child in node.children:
@@ -73,19 +82,21 @@ def find_prefix(root, prefix:str) -> Tuple[bool, int]:
                 node = child
                 break
         if char_not_found:
-            return False,0
+            return False, 0
     return True, node.counter
 
 
-# ipv4-el mukodik csak
-def pre_process_ip_addr(ip_addres):
-    # "192.168.0.100" formatumban erkezik be stringkent
-    # ezt szet kell szedni 4 darab 10es szamrendszerire
-    # majd ezeket int-e castolni es atalkitani binarissa
-    ip_addres_split = ip_addres.split('.')
+def pre_process_ip_addr(ip_address: str) -> str:
+    """
+    Preprocessing the IP address to binary format. The IP address is split by dots and each octet is converted to
+    binary format. It can be used only with IPv4 protocol.
+    :param ip_address:
+    :return:
+    """
+    ip_address_split = ip_address.split('.')
 
     output = ""
-    for octet in ip_addres_split:
+    for octet in ip_address_split:
         output += f'{int(octet):08b}'
 
     return output
